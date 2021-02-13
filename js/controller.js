f6acb31ce2fbd85f8e6b3db72e6e0285ddc35c6c
anubhavb11 <[anubhavb11@gmail.com]>
2021-02-13 23:34:29 +0530

    Added Detalis popup

--- js/controller.js
+++ js/controller.js
@@ -1,21 +1,45 @@
 const header = document.querySelector(".header");
 const top10Airhead = document.querySelector(".anime__airing__top-10__heading");
 const searchbtn = document.querySelector(".header__search-box__search__button");
-const search__id = document.getElementById("search__id");
+
 const searchResult__head = document.querySelector(
   ".anime__search__result__heading"
 );
 const sectionSearch = document.querySelector(".anime__search__result");
-
+const form = document.querySelector(".header__search-box__search");
+// let youtubecontainer;
 const modal = document.getElementById("myModal");
+
 // const jikanjs = require("jikanjs");
 // import jikanjs from "jikanjs";
 
 let modal_close;
+const videostop = function (youtubecontainer) {
+  youtubecontainer.src = " ";
+};
 
 // //////////////////GENERATE MARKUP////////////////
 
-const detailmarkup = ` <div class="modal-content">
+const detailmarkup = function (detail) {
+  const genresobj = detail.genres;
+  // console.log(genresobj);
+  const genres = [];
+  // let cnt = 0;
+  // let title = "";
+  for (let j = 0; j < genresobj.length; j++) {
+    genres.push(genresobj[j].name);
+  }
+  // console.log(genres);
+  let strgenre = " ";
+  for (let i = 0; i < genres.length; i++) {
+    strgenre += genres[i];
+    if (i < genres.length - 1) {
+      strgenre += ", ";
+    }
+  }
+  let youtubeURL = detail.trailer_url;
+
+  const markup = ` <div class="modal-content">
 <span class="close">&times;</span>
 
 <div class="details__modal">
@@ -23,7 +47,7 @@ const detailmarkup = ` <div class="modal-content">
          
 
     <h3 class="heading-primary">
-      Re:Zero kara Hajimeru Isekai Seikatsu 2nd Season Part 2
+      ${detail.title}
     </h3>
 
   </div>
@@ -32,7 +56,7 @@ const detailmarkup = ` <div class="modal-content">
     
     <div class="details__modal__head-1">
       <div class="details__modal__head-1__poster">
-        <img src="https://cdn.myanimelist.net/images/anime/1499/109126.jpg?s=3ec42e73c322253066dc78b01d70a" alt="" srcset="">
+        <img src=${detail.image_url} alt="" srcset="">
 
       </div>
 
@@ -41,10 +65,10 @@ const detailmarkup = ` <div class="modal-content">
      
       <div class="details__modal__head-1__rr">
         <!-- <div class="details__modal__head-1__rr__rating">
-          R - 17+ (violence & profanity)
+         ${detail.rating}
         </div> -->
         <div class="details__modal__head-1__rr__ranking">
-         Rank:  46
+         Rank: ${detail.rank}
         </div>
 
 
@@ -53,7 +77,7 @@ const detailmarkup = ` <div class="modal-content">
 
       <div class="details__modal__head-1__genre">
         <p>
-          Genre:Psychological,Drama,Thriller,<br>Fantasy
+          Genre:${strgenre}
         </p>
        
 
@@ -64,10 +88,11 @@ const detailmarkup = ` <div class="modal-content">
 
     </div>
     <div class="details__modal__head-2">
-      <div class="iframe-container">
+      <div  class="iframe-container">
         <iframe
+        class="youtube-vid"
           scrolling="no"
-          src=https://www.youtube.com/embed/6vMuWuWlW4I?enablejsapi=1&wmode=opaque&autoplay=1"
+          src=${youtubeURL}
         >
         </iframe>
       </div>
@@ -80,7 +105,7 @@ const detailmarkup = ` <div class="modal-content">
 <div class="details__modal__synopsis">
   <h2>Synopsis</h2>
   <p>
-    Ever since the death of his father, the burden of supporting the family 
+   ${detail.synopsis}
   </p>
 
 </div>
@@ -88,6 +113,9 @@ const detailmarkup = ` <div class="modal-content">
 
 
 </div>`;
+
+  return markup;
+};
 
 const loader = `  <div class="anime__search__result__loader">
 <img src="img/Cube.svg" alt="" srcset="" />
@@ -153,47 +181,42 @@ const generateMarkup = function (data) {
 const searchView = async function () {
   //   console.log("CLICKED");
   //   console.log(search__id.value);
-
-  let input = search__id.value;
+  const input = document.getElementById("search__id").value;
+  console.log("gll");
+  // let input = search__id.value;
+  console.log(input);
   if (input.length < 3) {
     alert("Please enter more than 3 characters");
     return;
   }
 
-  //   await header.insertAdjacentHTML("afterend", SearchMarkup);
-
-  // const firstElementChild = yearRangeToSelector.firstElementChild;
-  //   sectionSearch.innerHTML = " adeadesfcsefcdsfsfesfe";
-  //   while (sectionSearch.firstChild) {
-  //     sectionSearch.removeChild(sectionSearch.firstChild);
-  //     console.log("dewa");
-  //   }
   sectionSearch.innerHTML = " ";
   sectionSearch.insertAdjacentHTML("afterbegin", loader);
-
-  //   searchResult__head.innerHTML = " ";
-
-  // selectElement.append(firstElementChild);
-  //   sectionSearch.insertAdjacentHTML("afterbegin", SearchMarkup);
   function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
   await sleep(2000);
   sectionSearch.innerHTML = " ";
 
-  const res = await fetch(` https://api.jikan.moe/v3/search/anime?q=${input}`);
-  const data = await res.json();
+  try {
+    const res = await fetch(
+      ` https://api.jikan.moe/v3/search/anime?q=${input}`
+    );
+    const data = await res.json();
 
-  if (!res.ok) throw new Error(`${data.message}`);
-  console.log(data);
-  let x = data.results.slice(0, 10);
+    if (!res.ok) throw new Error(`${data.message}`);
+    console.log(data);
+    let x = data.results.slice(0, 10);
 
-  x.reverse().forEach(function (item, i) {
-    let markup = generateMarkup(item);
-    // console.log(markup);
-    // searchResult__head.insertAdjacentHTML("afterend", markup);
-    sectionSearch.insertAdjacentHTML("afterbegin", markup);
-  });
+    x.reverse().forEach(function (item, i) {
+      let markup = generateMarkup(item);
+      // console.log(markup);
+      // searchResult__head.insertAdjacentHTML("afterend", markup);
+      sectionSearch.insertAdjacentHTML("afterbegin", markup);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 /////////////////////LOADING PAGE WILL LOAD TOP 10 ARING ANIME//////////////////////////////////
@@ -209,57 +232,12 @@ const showTop10Airing = async function () {
     let top10Air = top50Air.slice(0, 50);
 
     for (let i = top10Air.length - 1; i >= 0; i--) {
-      //       let x = top10Air[i].title.split(" ");
-      //       //   console.log(x);
-      //       let cnt = 0;
-      //       let title = "";
-      //       for (let j = 0; j < x.length; j++) {
-      //         cnt = cnt + x[j].length;
-      //         if (cnt > 30) {
-      //           title += " ....";
-      //           break;
-      //         }
-      //         title = title + " " + x[j];
-      //       }
-      //       //   console.log(title);
-      //       const markup = `
-      //       <a href="${top10Air[i].mal_id}">
-      //        <div class="anime_card_parent">
-      //     <div class="anime__card card bg-dark text-white">
-      //     <div class="anime__card__box">
-      //       <img src=${top10Air[i].image_url} class="anime__card-img" alt="...">
-      //       <div class="anime__card__title">
-      //       <div class="anime__card__title__content">${title}</div>
-      //     </div>
-      //     </div>
-      //       <div class="anime__card__overlay">
-      //     <div class="anime__card__overlay_content">
-      //       <div class="anime__card__overlay_content__title"> <h3>${
-      //         top10Air[i].title
-      //       } </h3></div>
-      //       <div class="anime__card__overlay_content__score">
-      //         <h3>${top10Air[i].score} </h3>
-      //       </div>
-      //       <div class="anime__card__overlay_content__episodes"><h3> ${
-      //         top10Air[i].episodes == null
-      //           ? "Streaming..."
-      //           : `Episodes:  ${top10Air[i].episodes}`
-      //       } </h3></div>
-
-      //     </div>
-      //     </div>
-
-      //     </div>
-
-      //   </div>
-      //   </a>`;
-      //   console.log(markup);
       const testmark = generateMarkup(top10Air[i]);
 
       top10Airhead.insertAdjacentHTML("afterend", testmark);
     }
 
-    console.log(top10Air);
+    // console.log(top10Air);
   } catch (err) {
     console.log(err);
   }
@@ -267,29 +245,54 @@ const showTop10Airing = async function () {
 
 const openDetails = async function () {
   const id = window.location.hash.slice(1);
+
+  // console.log(id);
+  if (!id) {
+    return;
+  }
   modal.style.display = "block";
   modal.innerHTML = " ";
-  modal.insertAdjacentHTML("afterbegin", detailmarkup);
-  modal_close = document.getElementsByClassName("close")[0];
-  modal_close.addEventListener("click", closeModalWindow);
 
-  window.addEventListener("click", function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  });
   //  https://api.jikan.moe/v3/anime/${id}/
   try {
     const results = await axios({
       method: "GET",
-      url: `https://api.jikan.moe/v3/anime/${id}/`,
+
+      url: `https://api.jikan.moe/v3/anime/${id}`,
+      // // url:
+      //   "https://private-b5d8a26-jikan.apiary-mock.com/v3/season/2018/winter",
     });
-    console.log(results);
+    // console.log(results);
+    const detail = results.data;
+    // console.log(detail);
+    // detailmarkup(detail);
+    modal.insertAdjacentHTML("afterbegin", detailmarkup(detail));
   } catch (error) {
     console.log(error);
   }
   // console.log(x);
   // body.innerHTML = " ";
+  let youtubecontainer = document.querySelector(".youtube-vid");
+
+  modal_close = document.getElementsByClassName("close")[0];
+  modal_close.addEventListener("click", function () {
+    closeModalWindow();
+    videostop(youtubecontainer);
+  });
+  var scrollpos = this.scrollY;
+  console.log(scrollpos);
+
+  window.addEventListener("click", function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+    videostop(youtubecontainer);
+    localStorage.setItem(string(scrollpos));
+    console.log("THIS.......", scrollpos);
+
+    window.location.hash = "#";
+    // window.scrollTo(0, scrollpos);
+  });
 };
 
 const closeModalWindow = function () {
@@ -297,7 +300,10 @@ const closeModalWindow = function () {
 };
 
 /////////EVENT LISTNERs/////////////////////
-
-searchbtn.addEventListener("click", searchView);
+// console.log(searchbtn);
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  searchView();
+});
 showTop10Airing();
 window.addEventListener("hashchange", openDetails);
