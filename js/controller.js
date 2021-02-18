db920b3b331b11dfb90d8372e55bd4ebfd5a01a0
anubhavb11 <[anubhavb11@gmail.com]>
2021-02-18 11:17:58 +0530

    Added Various new Sections...

--- js/controller.js
+++ js/controller.js
@@ -3,8 +3,14 @@ const top10Airhead = document.querySelector(".anime__airing__top-10__heading");
 const top10upcominghead = document.querySelector(
   ".anime__upcoming__top-10__heading"
 );
+const top10moviecominghead = document.querySelector(
+  ".anime__movie__top-10__heading"
+);
+const animePopularhead = document.querySelector(".anime__popular__heading");
 
+// btn
 const searchbtn = document.querySelector(".header__search-box__search__button");
+const animeLoadMorebtn = document.querySelector(".anime__loadmore__btn");
 
 const searchResult__head = document.querySelector(
   ".anime__search__result__heading"
@@ -13,9 +19,6 @@ const sectionSearch = document.querySelector(".anime__search__result");
 const form = document.querySelector(".header__search-box__search");
 // let youtubecontainer;
 const modal = document.getElementById("myModal");
-
-// const jikanjs = require("jikanjs");
-// import jikanjs from "jikanjs";
 
 let modal_close;
 const videostop = function (youtubecontainer) {
@@ -125,9 +128,15 @@ const detailmarkup = function (detail) {
   return markup;
 };
 
-const loader = `  <div class="anime__search__result__loader">
+const loader = `  <div class="anime__search__result__loader ">
 <img src="img/Cube.svg" alt="" srcset="" />
 </div>`;
+
+const generatorLoader = function (cnt) {
+  return `  <div class="anime__search__result__loader loader__${cnt} ">
+  <img src="img/Cube.svg" alt="" srcset="" />
+  </div>`;
+};
 
 const SearchMarkup = `  <section class="anime__search__result">
 <div class="anime__search__result__heading">
@@ -170,6 +179,49 @@ const generateMarkup = function (data) {
     <div class="anime__card__overlay_content__episodes"><h3> ${
       data.episodes == null ? "Streaming..." : `Episodes:  ${data.episodes}`
     } </h3></div>
+
+
+  </div>
+  </div>
+
+  </div>
+
+
+</div>
+</a>`;
+
+  return markup;
+};
+const generateMarkupMovie = function (data) {
+  let x = data.title.split(" ");
+  //   console.log(x);
+  let cnt = 0;
+  let title = "";
+  for (let j = 0; j < x.length; j++) {
+    cnt = cnt + x[j].length;
+    if (cnt > 30) {
+      title += " ....";
+      break;
+    }
+    title = title + " " + x[j];
+  }
+  const markup = `
+    <a href="#${data.mal_id}">
+     <div class="anime_card_parent">
+  <div class="anime__card card bg-dark text-white">
+  <div class="anime__card__box">
+    <img src=${data.image_url} class="anime__card-img" alt="...">
+    <div class="anime__card__title">
+    <div class="anime__card__title__content">${title}</div>
+  </div>
+  </div>
+    <div class="anime__card__overlay">
+  <div class="anime__card__overlay_content">
+    <div class="anime__card__overlay_content__title"> <h3>${data.title} </h3></div>
+    <div class="anime__card__overlay_content__score">
+      <h3>${data.score} </h3>
+    </div>
+    <div class="anime__card__overlay_content__episodes"><h3> MOVIE </h3></div>
 
 
   </div>
@@ -250,6 +302,8 @@ const showTop10Airing = async function () {
     console.log(err);
   }
 };
+
+// //////////////////////TOP 10 Upcoming/////////////////
 const showTop10Upcomig = async function () {
   try {
     const res = await fetch("https://api.jikan.moe/v3/top/anime/1/upcoming");
@@ -265,6 +319,65 @@ const showTop10Upcomig = async function () {
       const testmark = generateMarkup(top10up[i]);
 
       top10upcominghead.insertAdjacentHTML("afterend", testmark);
+    }
+
+    // console.log(top10Air);
+  } catch (err) {
+    console.log(err);
+  }
+};
+///////////////////////top 10 anime movie/////////////////////
+const showTop10Movie = async function () {
+  try {
+    const res = await fetch("https://api.jikan.moe/v3/top/anime/1/movie");
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message}`);
+
+    // console.log(res, data);
+    let top50movie = data.top;
+    let top10movie = top50movie.slice(0, 10);
+
+    for (let i = top10movie.length - 1; i >= 0; i--) {
+      const testmark = generateMarkupMovie(top10movie[i]);
+
+      top10moviecominghead.insertAdjacentHTML("afterend", testmark);
+    }
+
+    // console.log(top10Air);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// ////////////////////////ANime POpular////////////////////////
+const showAnimePopular = async function (cnt) {
+  // animePopularhead.innerHTML = " ";
+  const loader = generatorLoader(cnt);
+  animePopularhead.insertAdjacentHTML("beforeend", loader);
+  const loaderAnimation = document.querySelector(`.loader__${cnt}`);
+
+  function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+  await sleep(2000);
+  // loader
+
+  // animePopularhead.innerHTML = " ";
+  try {
+    const res = await fetch(`https://api.jikan.moe/v3/top/anime/${cnt}`);
+    const data = await res.json();
+
+    loaderAnimation.classList.add("anime__search__result__loader__sleep");
+
+    if (!res.ok) throw new Error(`${data.message}`);
+
+    // console.log(res, data);
+    let top50up = data.top.reverse();
+    for (let i = top50up.length - 1; i >= 0; i--) {
+      const testmark = generateMarkup(top50up[i]);
+
+      animePopularhead.insertAdjacentHTML("beforeend", testmark);
     }
 
     // console.log(top10Air);
@@ -328,5 +441,12 @@ form.addEventListener("submit", function (event) {
 });
 showTop10Airing();
 showTop10Upcomig();
+showTop10Movie();
+showAnimePopular(1);
+let loadmrecnt = 2;
+animeLoadMorebtn.addEventListener("click", function (event) {
+  showAnimePopular(loadmrecnt);
+  loadmrecnt++;
+});
 window.addEventListener("hashchange", openDetails);
 // window.addEventListener("load", openDetails);
